31d117ccc63b97d6a4feb46b2362abae58602bdb
test <test@test.com>
2017-10-25 13:24:06 +0300

    [Studio] Delete screen "Kitty"

--- client/src/pages/Kitty/PhonePortrait/Kitty.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-kitty-phoneportrait',
-  templateUrl: 'Kitty.html'
-})
-export class Kitty_PhonePortrait extends Screen {
-}
