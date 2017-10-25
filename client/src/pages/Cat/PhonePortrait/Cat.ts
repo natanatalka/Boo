0759d53a91928bf28eb02dcb3f1352f4d4110fc2
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-25 13:22:29 +0300

    [Studio] Modified screen "Kitty"

--- client/src/pages/Cat/PhonePortrait/Cat.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-cat-phoneportrait',
-  templateUrl: 'Cat.html'
-})
-export class Cat_PhonePortrait extends Screen {
-}
---
+++ client/src/pages/Kitty/PhonePortrait/Kitty.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-kitty-phoneportrait',
+  templateUrl: 'Kitty.html'
+})
+export class Kitty_PhonePortrait extends Screen {
+}
